4058b35a5c074cf5dbc86069ba7aec1d2b61a9b6
Sooraj Sunil <soorajpro@gmail.com>
2022-11-24 15:41:31 -0500

    organized into readable files and folders

--- Functions/local_to_global.m
+++ Functions/local_to_global.m
@@ -1,30 +1,24 @@
 function [global_fix, global_mov] = local_to_global(local_fix, local_mov)
-
 % shift the local coordinates to origin
 local_fix.LocalOriginInWorld = [0, 0];
 local_mov.LocalOriginInWorld = [0, 0];
 % Find the max. size to approximately accomodate two maps
-
 xmax_fix = local_fix.XLocalLimits(2);
 ymax_fix = local_fix.YLocalLimits(2);
-
 xmax_mov = local_mov.XLocalLimits(2);
 ymax_mov = local_mov.YLocalLimits(2);
-
-xy_max  = ymax_fix + ymax_mov + xmax_fix + xmax_mov;
-%xy_max   = xmax_a + ymax_b;
-
-[global_fix] = resize_map(local_fix, xy_max);
-[global_mov] = resize_map(local_mov, xy_max);
-
+x_max = xmax_fix+(xmax_mov/2);
+y_max = ymax_fix+(ymax_mov/2);
+% Global coordinates
+[global_fix] = resize_map(local_fix, x_max, y_max);
+[global_mov] = resize_map(local_mov, x_max, y_max);
 end
 
-
-function [resized_map] = resize_map(map, xy_max)
+function [resized_map] = resize_map(map, x_max, y_max)
 % Get the resolution of the map
 res = map.Resolution;
 % Create an empty map with the same resolution as the input map
-resized_map = occupancyMap(xy_max, xy_max, res);
+resized_map = occupancyMap(x_max, y_max, res);
 % Find an approximate center to place the input map into the output frame
 ij = round(resized_map.GridSize./4);
 % Get the probability laye of the input map
--- mapFusionMAIN.m
+++ mapFusionMAIN.m
@@ -21,93 +21,34 @@ tmov_prob  = imwarp(occupancyMatrix(mov.map), tform, 'FillValues', ...
 % PROBABILITY GRID FUSION:
 [global_map] = bayes_grid_fusion(occupancyMatrix(fix.global), ....
     tmov_prob, fix.map.Resolution);
+%% PLOT
 
 % DISPLAY LOCAL MAPS:
-
-figure('Name','LOCAL MAP'); 
-show(fix.global); title('')
-xlabel('$x$ (m)','Interpreter','latex');
-ylabel('$y$ (m)','Interpreter','latex');
-
-figure('Name','FIXED MAP'); 
+figure('Name','LOCAL MAP - FIXED'); 
 show(fix.map); title('')
 xlabel('$x$ (m)','Interpreter','latex');
 ylabel('$y$ (m)','Interpreter','latex');
 
-figure('Name','MOVING MAP'); 
+figure('Name','LOCAL MAP - MOVING'); 
 show(mov.map); title('')
 xlabel('$x$ (m)','Interpreter','latex');
 ylabel('$y$ (m)','Interpreter','latex');
 
+% DISPLAY MATCHED INLIER POINTS:
+figure('Name','INLIER POINTS'); 
+showMatchedFeatures(i_fix, i_mov, inliers_fix, inlier_mov, ...
+    'method', 'montage', 'PlotOptions', {'mx', 'bx', 'y-'}); 
+legend('Matched inliers in fixed map','Matched inliers in moving map', ...
+    'Interpreter','latex');
+title(['Number of inliers = ' num2str(length(inliers_fix))], ...
+    'Interpreter','latex')
+set(gca, 'fontsize', 14,'FontName','Times New Roman')
 
-% % DISPLAY MATCHED INLIER POINTS:
-% figure('Name','INLIER POINTS'); 
-% showMatchedFeatures(i_fix, i_mov, inliers_fix, inlier_mov, ...
-%     'method', 'montage', 'PlotOptions', {'mx', 'bx', 'y-'}); 
-% legend('Matched inliers in fixed map','Matched inliers in moving map', ...
-%     'Interpreter','latex');
-% title(['Number of inliers = ' num2str(length(inliers_fix))], ...
-%     'Interpreter','latex')
-% set(gca, 'fontsize', 14,'FontName','Times New Roman')
-% 
-% % DISPLAY GLOBAL MAP:
-% figure('Name','GLOBAL MAP'); 
-% show(global_map); title('')
-% xlabel('$x$ (m)','Interpreter','latex');
-% ylabel('$y$ (m)','Interpreter','latex');
+% DISPLAY GLOBAL MAP:
+figure('Name','GLOBAL MAP'); 
+show(global_map); title('')
+xlabel('$x$ (m)','Interpreter','latex');
+ylabel('$y$ (m)','Interpreter','latex');
 
 rmpath('Functions/')
 rmpath('Data/')
-
-%%
-
-
-% clear map slamAlg 
-% 
-
-% 
-% 
-% 
-% [tform_ab, inPts_a, inPts_b] = mapFusionKAZE(I_free_a, I_free_b, m_a, m_b);
-% 
-% 
-% 
-% disp(tform_ab)
-% 
-% x_n = [inPts_a.inPts_a.Location(:,1), inPts_a.inPts_a.Location(:,2)]; 
-% y_n = [inPts_a.inPts_a.Location(:,1), inPts_a.inPts_a.Location(:,2)]; 
-
-% x_0 = [sum(x_n(:,1))/length(x_n), sum(x_n(:,2))/length(x_n)]; 
-% y_0 = [sum(y_n(:,1))/length(y_n), sum(y_n(:,2))/length(y_n)]; 
-% 
-% 
-% a_n   = (x_n - x_0)';
-% b_n   = (y_n - y_0)'; 
-% 
-% 
-% H = b_n*a_n';
-% 
-% [U, S, V] = svd(H);
-% 
-% 
-% R = V*U';
-% 
-% t = x_0' - R*y_0' ;
-% 
-% 
-% for k = 1:length(y_n)
-% x_bar(k,:) = R*y_n(k,1:2)' + t; 
-% end
-% 
-% tform = rigid2d(R,t');
-% 
-% disp(tform)
-% 
-% hold on 
-% plot(inPts_a.inPts_a.inPts_a.Location(:,1), inPts_a.inPts_a.inPts_a.Location(:,2), 'r+','MarkerSize',18)
-% %plot(inPts_b.Location(:,1), inPts_b.Location(:,2), 'bx')
-% plot(x_bar(:,1), x_bar(:,2), 'x')
-
-
-
-
